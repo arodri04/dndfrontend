5bab95d5582013fd4db417a49682e5af41566b4d
arodri04 <augustine.rodriguez55@gmail.com>
2024-07-02 15:01:34 -0600

    Started with dwarf

--- src/pages/Races/races.js
+++ src/pages/Races/races.js
@@ -1,12 +1,49 @@
-import React from "react"
+import React, { Fragment, useEffect, useState } from "react"
+import Axios from "axios"
 
 import MainNav from "../MainNav"
 
 function Races() {
+    const [isLoading, setLoading] = useState(true);
+    const [race, setRace] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
+    const fetchData = async() => {
+        await Axios.get('https://dndapi-4576af573829.herokuapp.com/races/Dwarf').then(({data}) => {
+            let race = data
+            setRace(race)
+            setLoading(false);
+            console.log(race.traits.content)
+        });
+        
+    }
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+
+
+    // for (let thing in race['Dwarf Traits'].content) {
+    //     console.log(race['Dwarf Traits'].content[thing])
+    // }
+
+
+    // Axios.get("https://dndapi-4576af573829.herokuapp.com/races/Dwarf").then((res) => {
+    //     let race = res.data;
+    //     console.log(race.name);
+    // });
+
     return (
-        <div className="container">
+        
+        <div>
             <MainNav />
-            <p>This is Races</p>
+            <p>{race.name}</p>
+            {race.traits.content.map((trait) => <p>{trait}</p>)}
+ 
         </div>
     )
 }
--- src/pages/Spells/spells.js
+++ src/pages/Spells/spells.js
@@ -3,7 +3,7 @@ import Axios from "axios"
 import MainNav from "../MainNav"
 
 function Spells() {
-            Axios.get("https://dndapi-4576af573829.herokuapp.com/spells/cure%20wounds").then((res) => {
+            Axios.get("https://dndapi-4576af573829.herokuapp.com/classes/barbarian").then((res) => {
             console.log(res.data);
         });
     
